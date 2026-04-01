e33df6209d0df7069ddb931e866c4c7eca591097
Chris J Collins <chris@hitorro.com>
2026-03-31 23:59:05 -0700

    app support

--- aoee/target/release/deps/errno-a68d12aba98236d3.d
+++ aoee/target/release/deps/errno-a68d12aba98236d3.d
@@ -1,8 +1,8 @@
-/Users/chris/AOEE/aoee/target/release/deps/liberrno-a68d12aba98236d3.rmeta: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
+/Users/chris/code/claude_world/social_enterprise/aoee/aoee/target/release/deps/liberrno-a68d12aba98236d3.rmeta: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
 
-/Users/chris/AOEE/aoee/target/release/deps/liberrno-a68d12aba98236d3.rlib: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
+/Users/chris/code/claude_world/social_enterprise/aoee/aoee/target/release/deps/liberrno-a68d12aba98236d3.rlib: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
 
-/Users/chris/AOEE/aoee/target/release/deps/errno-a68d12aba98236d3.d: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
+/Users/chris/code/claude_world/social_enterprise/aoee/aoee/target/release/deps/errno-a68d12aba98236d3.d: /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs
 
 /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/lib.rs:
 /Users/chris/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/errno-0.3.14/src/unix.rs:
